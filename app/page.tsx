c779da3bf4e76ce004a9fb100930c492870e7109
zhangjun <fzypzl@outlook.com>
2024-08-03 08:29:14 +0800

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import Image from "next/image";
+import {ProLayout} from "@ant-design/pro-components";
+
+export default function Home() {
+  return (
+      <ProLayout>
+        hello
+      </ProLayout>
+  );
+}
